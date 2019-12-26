2ab15bd31444c0efe3776d17256d0474d1b897c0
Jeff Barfield <jeff.barfield@butterfield.com>
2019-12-26 00:58:50 -0500

    Final Functionality Complete

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -28,7 +28,6 @@ import Home from "./Home";
 import Calendar from "./Calendar";
 import Settings from "./Settings";
 import Login from "./Login";
-import Logout from "./Logout";
 
 //API
 import HandleLogout from "../api/HandleLogout";
@@ -115,7 +114,7 @@ export default function Navigation(props) {
         setOpen(false);
     };
 
-    if (loginState === false) {
+    if (loginState === false || loginState === null) {
         return (
             <div className={classes.root}>
                 <CssBaseline />
@@ -138,7 +137,7 @@ export default function Navigation(props) {
                             <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" noWrap>
-                            Material App Skeleton
+                            Material App Skeleton | Logged Out
                         </Typography>
                     </Toolbar>
                 </AppBar>
@@ -234,7 +233,7 @@ export default function Navigation(props) {
                             <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" noWrap>
-                            Material App Skeleton
+                            Material App Skeleton | Logged In
                         </Typography>
                     </Toolbar>
                 </AppBar>
